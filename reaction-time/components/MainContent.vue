91f77f251c1635e7aa972ad5fcb560b99886ccd8
Mohammad Pouya Allahyari <pouya3883@gmail.com>
2022-07-04 16:34:06 +0430

    import components

---
+++ reaction-time/components/MainContent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <ClickMeBox />
+    <ClickMeButton />
+    <NameInput />
+    <StartButton />
+    <History />
+  </div>
+</template>
+
+<script>
+import ClickMeBox from '../components/ClickMeBox.vue'
+import ClickMeButton from '../components/ClickMeButton.vue'
+import NameInput from '../components/NameInput.vue'
+import StartButton from '../components/StartButton.vue'
+import History from '../components/History.vue'
+export default {
+  components: {
+    ClickMeBox,
+    ClickMeButton,
+    NameInput,
+    StartButton,
+    History,
+  },
+}
+</script>
+
+<style></style>
--- reaction-time/pages/index.vue
+++ reaction-time/pages/index.vue
@@ -1,9 +1,29 @@
 <template>
-  
+  <div>
+    <MainContent />
+  </div>
 </template>
 
 <script>
+import MainContent from '../components/MainContent.vue'
+
 export default {
-  
+  components: {
+    MainContent,
+  },
 }
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+body {
+  font-family: 'Open Sans', sans-serif;
+}
+</style>
